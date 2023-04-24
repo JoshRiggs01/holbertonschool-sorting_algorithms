7c0de46269b276abe4eb6274de231c69ea5c72cd
JoshRiggs01 <5574@holbertonstudents.com>
2023-04-23 22:07:41 -0700

    removing insertion and sort

--- sort.h
+++ sort.h
@@ -8,7 +8,7 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 //void insertion_sort_list(listint_t **list);
-void print_list(const listint_t *list);
+//void print_list(const listint_t *list);
 /**
  * struct listint_s - Doubly linked list node
  *
