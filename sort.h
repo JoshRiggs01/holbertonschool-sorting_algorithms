6e8f52be44fa2ea4dc64f22da1d359f628555374
JoshRiggs01 <5574@holbertonstudents.com>
2023-04-23 22:16:09 -0700

    donno if thisll help

--- sort.h
+++ sort.h
@@ -2,13 +2,8 @@
 #define sort_h
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 
-void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-//void insertion_sort_list(listint_t **list);
-//void print_list(const listint_t *list);
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,4 +17,12 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 #endif
