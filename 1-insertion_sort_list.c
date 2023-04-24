99bf046d7153bc9dc74bdfe75e89f393e0e19b2f
JoshRiggs01 <5574@holbertonstudents.com>
2023-04-23 22:26:01 -0700

    updating insertions sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,46 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sorts a doubly linked list
- * @list: pointer to pointer where list is
- *
- * Return: void
- */
+* swapNodes - swaps two node links that are next to each other
+*@list: a doubly linked list
+*@node1: first node to switch
+*@node2: second node to switch
+*/
+
+    void swapNodes(listint_t **h, listint_t **n1, listint_t *n2)
+    {
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+    }
+
+/**
+* insertion_sort_list - sort a doubly linked list in ascending order
+* @list: the list to be sorted
+*/
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = *list;
+    listint_t *iter, *insert, *tmp;
 
-	while (curr)
-	{
-		while (curr->prev && curr->prev->n > curr->n)
-		{
-			curr->prev->next = curr->next;
-			if (curr->next)
-			{
-				curr->next->prev = curr->prev;
-			}
-			curr->next = curr->prev;
-			curr->prev = curr->prev->prev;
-			curr->next->prev = curr;
-			if (curr->prev)
-			{
-				curr->prev->next = curr;
-			}
-			else
-			{
-				*list = curr;
-			}
-			print_list(*list);
-			printf("\n");
-		}
-		curr = curr->next;
-	}
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+    for (iter = (*list)->next; iter != NULL; iter = tmp)
+    {
+        tmp = iter->next;
+        insert = iter->prev;
+        while (insert != NULL && iter->n < insert->n)
+        {
+            swapNodes(list, &insert, iter);
+            print_list((const listint_t *)*list);
+        }
+    }
 }
